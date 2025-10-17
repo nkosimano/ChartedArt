fc3f7a7fe46972de0929e0ad053ff90f8731c27e
Immanuel <nkosimano@gmail.com>
2025-10-17 14:32:46 +0200

    chore: add TypeScript support and additional Babel plugins with loose mode enabled

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,18 +1,31 @@
 module.exports = function(api) {
   api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          alias: {
-            '@': './src'
-          }
+  
+  const presets = [
+    'babel-preset-expo',
+    '@babel/preset-typescript',
+  ];
+
+  const plugins = [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src'
         }
-      ],
-      'react-native-reanimated/plugin'
-    ]
+      }
+    ],
+    'react-native-reanimated/plugin',
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-nullish-coalescing-operator', { loose: true }],
+    ['@babel/plugin-transform-optional-chaining', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }]
+  ];
+
+  return {
+    presets,
+    plugins,
   };
 };
